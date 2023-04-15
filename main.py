365cc96f4e9903856bfcf60ecc8d6628e42b99ea
Reynaldi Chernando <reynaldichernando@gmail.com>
2023-04-15 16:00:10 +0700

    Adjust script

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
+import os
+
+directory = "securimage-data"
 
 def main():
+    if not os.path.exists(directory):
+        os.mkdir(directory)
     s = requests.Session()
     for i in range(10000):
         if i % 200 == 0:
@@ -8,7 +13,7 @@ def main():
         securimage_show = s.get("http://localhost:8000/securimage_show.php")
         securimage_code = s.get("http://localhost:8000/securimage_code.php")
 
-        with open("securimage-data/{}.png".format(securimage_code.text), "wb") as f:
+        with open("{}/{}.png".format(directory, securimage_code.text), "wb") as f:
             f.write(securimage_show.content)
 
 if __name__ == "__main__":
