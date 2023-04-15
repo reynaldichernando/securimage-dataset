33f10614bdd0d32a9e2ec2b0957f9eba7b61648e
Reynaldi Chernando <reynaldichernando@gmail.com>
2023-04-15 15:32:44 +0700

    Init code

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+def main():
+    s = requests.Session()
+    for i in range(10000):
+        if i % 200 == 0:
+            print(i)
+        securimage_show = s.get("http://localhost:8000/securimage_show.php")
+        securimage_code = s.get("http://localhost:8000/securimage_code.php")
+
+        with open("securimage-data/{}.png".format(securimage_code.text), "wb") as f:
+            f.write(securimage_show.content)
+
+if __name__ == "__main__":
+    main()
